9dd4495e5faaf2a8d741f6a2420f8402b6af8019
John Dempsey <john.dempsey@iadt.ie>
2026-03-25 11:12:17 +0000

    Finishing JS events

---
+++ src/exercises/08-js-events/03-delegation.js
@@ -0,0 +1,34 @@
+// console.log("Ouch!");
+
+let cardsContainer = document.getElementById("cards");
+
+function handleClicks(event) {
+    // console.log(`You clicked on a ${event.currentTarget.tagName} element\n`);
+    // console.log(`You clicked on a ${event.target.tagName} element\n`);
+
+    const card = event.target.closest('.card');
+    
+    if (!card) {
+        return;
+    }
+
+    const action = event.target.dataset.action;
+    if (action === "select") {
+        // console.log("You clicked on a select button");
+        toggleCardHighlight(card);
+    }
+    else if (action === "log") {
+        // console.log("You clicked on a log button");
+        logCardTitle(card);
+    }
+}
+
+function toggleCardHighlight(card) {
+    card.classList.toggle('selected');
+}
+
+function logCardTitle(card) {
+    console.log('Card title: ', card.dataset.title);
+}
+
+cardsContainer.addEventListener('click', handleClicks);
